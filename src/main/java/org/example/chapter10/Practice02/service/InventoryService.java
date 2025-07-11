698934302bcdc6a021eec84b7a70c31dc99bb6e4
minion-K <rnjsalsgus06@naver.com>
2025-07-11 17:02:07 +0900

    0711 Practice(Item)

---
+++ src/main/java/org/example/chapter10/Practice02/service/InventoryService.java
@@ -0,0 +1,12 @@
+package org.example.chapter10.Practice02.service;
+
+import org.example.chapter10.Practice02.entity.Item;
+
+import java.util.List;
+
+public interface InventoryService {
+    void addItem(Item item);
+    List<Item> getItemsByCategory(String category);
+    boolean updateItemPrice(String itemId, int newPrice);
+    void deleteItem(String itemId);
+}
