c17cddebfc5f14e5ccc435076718e9487e7469c4
minion-K <rnjsalsgus06@naver.com>
2025-07-11 12:35:33 +0900

    0711 Practice(Book)

--- src/main/java/org/example/chapter10/Practice01/LibraryManager.java
+++ src/main/java/org/example/chapter10/Practice01/LibraryManager.java
@@ -15,18 +15,55 @@ public class LibraryManager implements Manageable{
     }
 
     @Override
-    public void remove(String id) {
-
+    public void listAll() {
+        // 전체 조회: isEmpty()로 요소값 존재 여부 확인에 따라 로직 철
+        if(items.isEmpty()) {
+            System.out.println("No Items is the library");
+        } else {
+            System.out.println(" == Library Items ==");
+            for(Item item: items) {
+                item.display();
+            }
+        }
     }
 
     @Override
     public void updateStock(String id, int quantity) {
+        for(Item item: items) {
+//            item.updateStock(quantity);
+            if(item.getId().equals(id) && item instanceof Book) {
+//                Book selectedBook = (Book)item;
 
+                ((Book) item).updateStock(quantity);
+//                selectedBook.updateStock(quantity);
+
+                System.out.println("Stock updated for ID: " + id + ", Stock: " + ((Book)item).getStock());
+//                System.out.println("Stock updated for ID: " + id + ", Stock: " + selectedBook.getStock());
+            }
+        }
     }
 
     @Override
-    public void listAll() {
+    public void remove(String id) {
+//        삭제 여부 확인용 boolean 데이터
+        boolean removed = false;
+        
+//        인덱스 번호를 사용하여 요소 제거
+//        : 일반 for문을 초기화 변수 == 인덱스 번호
+        int size = items.size();
+        for(int i = 0; i < size; i++) {
+//            전체 items를 순회: 각 순번의 item의 id와 매개변수의 id가 일치한다면 해당 인덱스 요소를 제거
+            if(items.get(i).getId().equals(id)) {
+                items.remove(i);
+                removed = true;
+                System.out.println("Item with ID: " + id + " removed");
+                break;
+            }
+        }
 
+        if(!removed) {
+            System.out.println("No Item found with ID: " + id);
+        }
     }
 
     @Override
