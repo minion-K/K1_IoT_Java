3f4edaf60433085871140e5817373fb4db0dfa8d
minion-K <rnjsalsgus06@naver.com>
2025-07-24 11:01:00 +0900

    0724 DATA

---
+++ src/main/java/org/example/chapter16/D_Data.java
@@ -0,0 +1,86 @@
+package org.example.chapter16;
+
+/*
+* === DTO, VO, DAO, Entity ===
+* : 데이터를 목적에 따라 정의하는 구조
+* 
+* 1) DTO(Data Transfer Object): 데이터를 전달하기 위한 객체
+* 2) VO(Value Object): 값 자체를 표현하는 객체
+* 3) DAO(Data Access Object): Database에 접근하는 역할을 하는 객체
+* 4) Entity: 실제 DB 테이블과 매핑되는 클래스
+* */
+
+import lombok.*;
+
+// 1) DTO
+// : 계층 간 데이터 교환을 위해 사용하는 객체
+// - 로직을 가지지 않는 순수한 데이터 객체 (비즈니스 로직 X, Getter & Setter만 존재)
+// - 요청(Request)과 응답(Response) 객체로 구분하여 사용
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+class UserSignInRequestDto {
+//    로그인을 위한 요청값
+    private String userId;
+    private String password;
+}
+
+// 2) VO
+// : 특정 값을 표현하는 객체
+// - 주로 사용자 주소, 이름, 좌표 등 도메인에서 의미있는 값을 표현
+// - 불변성(읽기 전용): 생성 시 내용 변경 불가
+// >> 주로 필드값은 final, Getter 만을 가짐
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+class Address {
+//    데이터 식별 값 필요 X (ID가 없음)
+//    동일성 보다 동등성이 중요 (VO는 값이 같으면 동일하다고 봄)
+    private final String city;
+    private final String zipcode;
+}
+
+// 3) DAO
+// : 데이터베이스 접근 객체 
+// - 서비스 모델과 데이터베이스를 연결하는 역할
+// - DB에 접근해서 CRUD 작업을 담당
+// >> 일반적으로 JDBC 또는 JPA, MyBatis 등을 통해 구현
+// >> Spring 기반 프로젝트에서 Repository 또는 Mapper로 대체
+
+public class D_Data {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
